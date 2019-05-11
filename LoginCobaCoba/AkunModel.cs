127f5969a5e45a5532232203757c5b2e58583fa5
Firhan Mahdi Ramadhani <46242902+fearhunt@users.noreply.github.com>
2019-05-11 10:39:57 +0700

    Add files via upload

---
+++ LoginCobaCoba/AkunModel.cs
@@ -0,0 +1,23 @@
+namespace LoginCobaCoba
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class AkunModel : DbContext
+    {
+        public AkunModel()
+            : base("name=AkunModel")
+        {
+        }
+
+        public virtual DbSet<Akun> Akun { get; set; }
+        public virtual DbSet<Table> Table { get; set; }
+        public virtual DbSet<Table2> Table2 { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
